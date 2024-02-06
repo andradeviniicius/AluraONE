97fb9b9342c23556cf53a6011ee3be1b18343014
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2024-02-06 20:50:54 -0300

    feat/ created encrypt function

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let encryptButton = document.querySelector(".btn-encrypt");
 let decryptButton = document.querySelector(".btn-decrypt");
 
 function encryptMessage(string) {
-  console.log(string.split(""));
   let newString = string.split("").map((el) => {
     if (vowels.includes(el.toLowerCase())) {
       return el + "d4n4n4";
@@ -26,17 +25,20 @@ let empty_message_warning = document.querySelector(".empty-messages-warning");
 let encrypted_message = document.querySelector(".encrypted-message");
 let encrypt_message_container = document.querySelector(".results-container");
 let result_text = document.querySelector(".encrypted-message-result");
+let user_input = document.querySelector("#inputDecodificador");
 
 encryptButton.addEventListener("click", () => {
+  if (user_input.value == "") {
+    alert("Você precisa inseri um valor para funcionar :)");
+    return;
+  }
   empty_message_warning.style.display = "none";
   encrypted_message.style.display = "flex";
-
-  encrypt_message_container;
+  result_text.innerHTML = encryptMessage(user_input.value);
 });
 
 decryptButton.addEventListener("click", () => {
-  empty_message_warning.style.display = "flex";
-  encrypted_message.style.display = "none";
+  alert("descripto");
 });
 
 function copyToClipboard() {
